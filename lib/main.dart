01f3ab250117d8c0ac3d010303b11fc4a62b8ea9
Fakeha Rahman <fakeha128@gmail.com>
2019-06-08 15:14:36 +0530

    Flutter tutorial

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+//if a function is inside a class its called a method
+  @override
+  build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Easy list'),
+        ),
+        body: Card(child: Column(children: <Widget>[
+          Image(),
+          Text('Food paradise')
+
+        ],),),
+      ),
+    );
+
+    //for the theaming and shit. Used in every flutter app // ctrl+space to see the options in a widget paranthasis
+  }
+}
